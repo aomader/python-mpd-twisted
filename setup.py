2ef9135b7de80f0d5cbef7df7e7aad7442a822ae
Oliver Mader <b52@reaktor42.de>
2010-10-10 14:07:37 +0200

    No documentation is better then a wrong one

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from distutils.core import setup
 
-
 DESCRIPTION = """
-An MPD (Music Player Daemon) client library written in pure Python, using Twisted
+A MPD (Music Player Daemon) client library written in pure Python, using Twisted
 """
 
 CLASSIFIERS = [
@@ -17,8 +17,10 @@ CLASSIFIERS = [
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
 
-LICENSE = """\
+LICENSE = """
 Copyright (C) 2008  J. Alexander Treuman <jat@spatialrift.net>
+Copyright (C) 2010  Jasper St. Pierre <jstpierre@mecheye.net>
+Copyright (C) 2010  Oliver Mader <b52@reaktor42.de>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -34,22 +36,19 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
 setup(
     name="python-mpd-twisted",
-    version="0.2.1",
+    version="0.3",
     description="Python MPD client library using Twisted",
     long_description=DESCRIPTION,
-    author="J. Alexander Treuman, JP St. Pierre",
-    author_email="jat@spatialrift.net, jstpierre@mecheye.net",
-    url="http://github.com/magcius/python-mpd",
-    download_url="http://github.com/magcius/python-mpd",
+    author="J. Alexander Treuman, JP St. Pierre, Oliver Mader",
+    author_email="jat@spatialrift.net, jstpierre@mecheye.net, b52@reaktor42.de",
+    url="https://code.reaktor42.de/projects/python-mpd-twisted",
+    download_url="https://code.reaktor42.de/projects/python-mpd-twisted",
     py_modules=["mpd"],
     classifiers=CLASSIFIERS,
-    #license=LICENSE,
+    license=LICENSE,
     keywords=["mpd"],
     #platforms=["Independant"],
 )
 
-
-# vim: set expandtab shiftwidth=4 softtabstop=4 textwidth=79:
